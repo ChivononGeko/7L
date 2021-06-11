b6bd7158d97befdb44f9c3e14688458e2adb4b57
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-06-11 12:28:48 +0600

    Add files via upload

---
+++ 7.6/7.6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace l7t6
+{
+    /*
+    * Реализуйте класс Cat, он должен:
+    * 1) Содержать свойство Id.
+    * 2) Содержать конструктор, который принимает один целочисленный параметр и присваивает его свойству.
+    * 3) Переопределите метод ToString() так, чтобы он выводил фразу "Я котик и мой id = XXX". XXX - Id экземпляра класса.
+    * Внутри метода Main() создайте коллекцию экземпляров класса Cat. Создайте 100 экземпляров класса Cat, а
+    * в созданную коллекцию добавьте только котов с четными значениями id.
+    * Выведите на экран содержимое коллекции, каждое значение с новой строки. Циклом foreach пользоваться нельзя.
+    */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            List<Cat> cats = new List<Cat>();
+            for (int i = 0; i < 100; i++)
+            {
+               Cat cat = new Cat(i);
+                if (i%2 == 0 )
+                {
+                    cats.Add(cat);
+                }
+            }
+            for (int i = 0; i < cats.Count; i++)
+            {
+                Console.WriteLine(cats[i]);
+            }
+        }
+    }
+    public class Cat
+    {
+        /* Добавьте свой код ниже */
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { this.id = value; }
+        }
+        public Cat(int id)
+        {
+            this.Id = id;
+        }
+
+        public override string ToString()
+        {
+            return $"Ya kotik moy id = {this.Id}";
+        }
+    }
+}
---
+++ 7.8/7.8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace l7t8
+{
+    /*
+    * Внутри метода Main() дан список строк list. Реализуйте следующую логику работы со списком:
+    * 1) Удалите из списка все слова, содержащие букву Т.
+    * 2) Все слова содержащие букву Б сделайте в ВЕРХНЕМ РЕГИСТРЕ.
+    * 3) Во всех словах, содержащих букву Р - замените её на букву Л.
+    * Шаблон задачи изменять нельзя.
+    */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "собака", "улыбака", "мурка", "кот", "бурка", "Сивка", "баркаc" };
+            /* Добавьте свой код ниже */
+            for (int i = 0; i < list.Count; i++)
+            {
+                char[] a = list[i].ToCharArray();
+                for (int j = 0; j < a.Length; j++)
+                {
+                                      
+                    if (Convert.ToString(a[j]) == "т")
+                    {
+                        list.RemoveAt(i);
+                        i--;
+                    }
+                    if (Convert.ToString(a[j]) == "б")
+                    {
+                       list[i] = list[i].ToUpper();
+                    }
+                    if (Convert.ToString(a[j]) == "р")
+                    {
+                       a[j] = Convert.ToChar("л");
+                    }
+                }
+                
+            }
+            foreach (var str in list)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
