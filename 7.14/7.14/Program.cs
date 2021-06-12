8a048ec9347f4d1b944e15793e70b38518d15892
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-06-12 16:00:31 +0600

    Add files via upload

---
+++ 7.14/7.14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace l7t14
+{
+    /*
+    * Внутри метода Main() создайте словарь и заполните его 10 парами ключ-значение, введенными с клавиатуры.
+    * Выведите на экран элементы словаря в следующем формате:
+    * "key - value", где key и value соответствующие значения элемента словаря.
+    */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            Dictionary<string, string> vk = new Dictionary<string, string>(10);
+            for (int i = 0; i < 10; i++)
+            {
+                vk.Add(Console.ReadLine(), Console.ReadLine());
+            }
+            foreach (KeyValuePair<string, string> keyValue in vk)
+            {
+                Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
+            }
+        }
+    }
+}
